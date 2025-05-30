0d2c87101599461dc93342a3d8c9f790b447979f
Eric <emp2332@gmail.com>
2025-05-30 18:57:32 -0400

    carter assemblies

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -3,30 +3,30 @@ using Mapster;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
-public record CreateProductRequest(Guid Id,
-                                   string Name,
-                                   List<string> Category,
-                                   string Description,
-                                   string ImageFile,
-                                   decimal Price);
+    public record CreateProductRequest(Guid Id,
+                                       string Name,
+                                       List<string> Category,
+                                       string Description,
+                                       string ImageFile,
+                                       decimal Price);
 
-public record CreateProductResponse(Guid Id);
+    public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint : ICarterModule
-{
-    public void AddRoutes(IEndpointRouteBuilder app)
+    public class CreateProductEndpoint : ICarterModule
     {
-        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
-            {
-                CreateProductCommand command = request.Adapt<CreateProductCommand>();
-                CreateProductResult result = await sender.Send(command);
-                CreateProductResult response = result.Adapt<CreateProductResult>();
-                return Results.Created($"/products/{response.Id}", response);
-            })
-           .WithName("CreateProduct")
-           .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-           .ProducesProblem(StatusCodes.Status400BadRequest)
-           .WithSummary("Create Product")
-           .WithDescription("Create Product");
-    }
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+                {
+                    CreateProductCommand command = request.Adapt<CreateProductCommand>();
+                    CreateProductResult result = await sender.Send(command);
+                    CreateProductResult response = result.Adapt<CreateProductResult>();
+                    return Results.Created($"/products/{response.Id}", response);
+                })
+               .WithName("CreateProduct")
+               .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+               .ProducesProblem(StatusCodes.Status400BadRequest)
+               .WithSummary("Create Product")
+               .WithDescription("Create Product");
+        }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,18 @@
+using Carter;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(assemblies: typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 // deps
 WebApplication app = builder.Build();
 
-// middleware
+// http request pipeline
+app.MapCarter();
 
 
 app.Run();
